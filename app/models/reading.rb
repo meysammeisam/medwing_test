67d010f142b99353e42bf7e06944b25574ec9d5f
MeysaM <meysammeisam@gmail.com>
2019-03-16 16:02:27 +0330

    Add model validations

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,2 +1,7 @@
 class Reading < ApplicationRecord
+  belongs_to :thermostat
+
+  validates :thermostat, :temperature, :humidity, :battery_charge, :seq_number, presence: true
+  validates :temperature, :humidity, :battery_charge, numericality: true
+  validates :seq_number, numericality: { greater_than_or_equal_to: 1 }, uniqueness: { scope: :thermostat_id }
 end
--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -1,2 +1,5 @@
 class Thermostat < ApplicationRecord
+  has_many :readings
+
+  validates :household_token, :location, presence: true, uniqueness: true
 end
